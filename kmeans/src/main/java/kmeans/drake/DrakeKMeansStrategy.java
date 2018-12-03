164b5ac5c264368588cd480122b07cc0b6fc7cdf
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2018-12-03 23:57:14 +0100

    start drake, not very far though

--- kmeans/src/main/java/kmeans/drake/DrakeKMeansStrategy.java
+++ kmeans/src/main/java/kmeans/drake/DrakeKMeansStrategy.java
@@ -24,6 +24,11 @@ public class DrakeKMeansStrategy extends KMeansStrategy {
    */
   private double[] upperBounds;
 
+  /**
+   * stores the number of bounds (changes over time)
+   */
+  private int B;
+
   @Override
   public Cluster[] cluster(double[][] dataPoints, double[][] initialClusterCenters, int maxNumberOfIterations,
       DistanceStrategy distance) {
@@ -31,6 +36,7 @@ public class DrakeKMeansStrategy extends KMeansStrategy {
     this.D = dataPoints[0].length;
     this.K = initialClusterCenters.length;
     this.N = dataPoints.length;
+    this.B = 1;
 
     this.clusterAssignments = new int[N];
     this.clusterCenters = Arrays.stream(initialClusterCenters).map(double[]::clone).toArray(double[][]::new);
@@ -49,50 +55,47 @@ public class DrakeKMeansStrategy extends KMeansStrategy {
 
     while (hasChanged && numberOfIterations < maxNumberOfIterations) {
       hasChanged = false;
-      for (int k = 0; k < K; k++) {
-        double closestClusterDistance = Double.MAX_VALUE;
-        for (int l = 0; l < K; l++) {
-          if (k != l) {
-            closestClusterDistance = Math.min(closestClusterDistance,
-                this.distance.compute(clusterCenters[k], clusterCenters[l]));
-          }
-        }
-      }
+
+      // outerLoop: for (int n = 0; n < N; n++) {
+      // for (int b = 0; b < B; b++) {
+      // if (upperBounds[n] <= lowerBounds[n][b]) {
+      // continue outerLoop;
+      // }
+      // }
+      // }
 
       // update centers
       for (int k = 0; k < K; k++) {
         if (clusterSizes[k] > 0) {
+          final double[] newClusterCenter = new double[D];
           for (int d = 0; d < D; d++) {
             double newClusterCenterCoordinate = clusterSums[k][d] / clusterSizes[k];
+            newClusterCenter[d] = newClusterCenterCoordinate;
           }
+          clusterCentersDistanceMoved[k] = this.distance.compute(clusterCenters[k], newClusterCenter);
           hasChanged = hasChanged || clusterCentersDistanceMoved[k] > 0;
+          System.arraycopy(newClusterCenter, 0, clusterCenters[k], 0, D);
         } else {
           throw new IllegalArgumentException("this is rare");
         }
       }
 
       // update bounds
-      int mostDistanceMovedIndex = -1;
-      double mostDistanceMoved = Double.MIN_VALUE;
-      double secondMostDistanceMoved = Double.MIN_VALUE;
-      for (int k = 0; k < K; k++) {
-        double currentDistanceMoved = clusterCentersDistanceMoved[k];
-        if (currentDistanceMoved > mostDistanceMoved) {
-          secondMostDistanceMoved = mostDistanceMoved;
-          mostDistanceMoved = currentDistanceMoved;
-          mostDistanceMovedIndex = k;
-        } else if (currentDistanceMoved > secondMostDistanceMoved) {
-          secondMostDistanceMoved = currentDistanceMoved;
-        }
-      }
-      for (int n = 0; n < N; n++) {
-        upperBounds[n] += clusterCentersDistanceMoved[clusterAssignments[n]];
-        if (mostDistanceMovedIndex == clusterAssignments[n]) {
-          lowerBounds[n] -= secondMostDistanceMoved;
-        } else {
-          lowerBounds[n] -= mostDistanceMoved;
-        }
-      }
+      // double mostDistanceMoved = Double.MIN_VALUE;
+      // for (int k = 0; k < K; k++) {
+      // mostDistanceMoved = Math.max(mostDistanceMoved,
+      // clusterCentersDistanceMoved[k]);
+      // }
+      // for(int n=0;n<N;n++){
+      // upperBounds[n]+=clusterCentersDistanceMoved[first];
+      // lowerBounds[n][B-1]-=mostDistanceMoved;
+      // for(int b=B-2;b>=0;b--){
+      // lowerBounds[n][b]-=clusterCentersDistanceMoved[assignments2[n,b]];
+      // if(lowerBounds[n][b] > lowerBounds[n][b+1]){
+      // lowerBounds[n][b] = lowerBounds[n][b+1];
+      // }
+      // }
+      // }
 
       numberOfIterations++;
     }
@@ -105,29 +108,16 @@ public class DrakeKMeansStrategy extends KMeansStrategy {
   }
 
   private void initialize() {
-    for (int n = 0; n < N; n++) {
-      int closestClusterCenterIndex = -1;
-      double closestClusterCenterDistance = Double.MAX_VALUE;
-      double secondClosestClusterCenterDistance = Double.MAX_VALUE;
-      for (int k = 0; k < K; k++) {
-        double currentClusterCenterDistance = this.distance.compute(dataPoints[n], clusterCenters[k]);
-        if (currentClusterCenterDistance < closestClusterCenterDistance) {
-          secondClosestClusterCenterDistance = closestClusterCenterDistance;
-          closestClusterCenterDistance = currentClusterCenterDistance;
-          closestClusterCenterIndex = k;
-        } else if (currentClusterCenterDistance < secondClosestClusterCenterDistance) {
-          secondClosestClusterCenterDistance = currentClusterCenterDistance;
-        }
-      }
 
-      clusterAssignments[n] = closestClusterCenterIndex;
-      upperBounds[n] = closestClusterCenterDistance;
-      lowerBounds[n] = secondClosestClusterCenterDistance;
+  }
 
-      clusterSizes[clusterAssignments[n]]++;
-      for (int d = 0; d < D; d++) {
-        clusterSums[clusterAssignments[n]][d] += dataPoints[n][d];
-      }
+  private void sortCenters(int n, int numberOfBounds, double[][] clusterCenters) {
+    Arrays.sort(clusterCenters,
+        (a, b) -> this.distance.compute(a, dataPoints[n]) > this.distance.compute(b, dataPoints[n]) ? 1 : -1);
+    var first = clusterCenters[0];
+    upperBounds[n] = this.distance.compute(dataPoints[n], first);
+    for (int i = 0; i < numberOfBounds; i++) {
+      // clusterAssignments
     }
   }
 }
