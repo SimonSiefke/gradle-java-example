a2a6e0de8f94ce22fdc58304836dc9f4e1dc2c6a
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2018-12-03 22:29:59 +0100

    add comment

--- kmeans/src/main/java/util/Util.java
+++ kmeans/src/main/java/util/Util.java
@@ -5,49 +5,18 @@ package util;
  */
 public final class Util {
   /**
-   * updates the cluster centers to the mean of the points that are assigned to
-   * each cluster.
+   * Assigns a point to a cluster.
    *
-   * @param originalClusterCenters the original cluster centers
-   * @param copyToClusterCenters   the cluster centers that will be updated based
-   *                               on the values by the original cluster centers.
-   *                               OriginalClusterCenters and copyToClusterCenters
-   *                               can be the same.
-   * @param clusterAssignments     indices of which point is assigned to which
-   *                               cluster
-   * @param dataPoints             the data points
-   * @return if one or more of the clusters have changed
+   * @param clusterAssignments   - the cluster assignments
+   * @param n                    - the data point index (the old cluster
+   *                             assignment index is clusterAssignments[n])
+   * @param newClusterAssignment - the new cluster assignment index
+   * @param clusterSizes         - the sizes of every cluster
+   * @param clusterSums          - the sum of the points assigned to each cluster
+   *                             center (for each dimension).
+   * @param D                    - the number of dimensions
+   * @param dataPoints           - the data points
    */
-  public static boolean updateClusterCenters(double[][] originalClusterCenters, double[][] copyToClusterCenters,
-      int[] clusterAssignments, double[][] dataPoints) {
-    final int K = originalClusterCenters.length;
-    final int N = dataPoints.length;
-    final int D = dataPoints[0].length;
-    final double[][] clusterSums = new double[K][D];
-    boolean hasChanged = false;
-    final int[] numberOfPointsInClusters = new int[K];
-
-    for (int n = 0; n < N; n++) {
-      numberOfPointsInClusters[clusterAssignments[n]]++;
-      for (int d = 0; d < D; d++) {
-        clusterSums[clusterAssignments[n]][d] += dataPoints[n][d];
-      }
-    }
-
-    for (int k = 0; k < K; k++) {
-      if (numberOfPointsInClusters[k] > 0) {
-        for (int d = 0; d < D; d++) {
-          double newClusterCenterCoordinate = clusterSums[k][d] / numberOfPointsInClusters[k];
-          hasChanged = hasChanged || originalClusterCenters[k][d] != newClusterCenterCoordinate;
-          copyToClusterCenters[k][d] = newClusterCenterCoordinate;
-        }
-      } else {
-        copyToClusterCenters[k] = originalClusterCenters[k];
-      }
-    }
-    return hasChanged;
-  }
-
   public static void assignPointToCluster(int[] clusterAssignments, int n, int newClusterAssignment, int[] clusterSizes,
       double[][] clusterSums, int D, double[][] dataPoints) {
     if (clusterAssignments[n] != newClusterAssignment) {
@@ -60,8 +29,6 @@ public final class Util {
       clusterAssignments[n] = newClusterAssignment;
     }
   }
-
-  // TODO cluster assignments to clusters
 
   private Util() {
     // private constructor to prevent creating objects of this class.
