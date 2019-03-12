7196625274a0c575311c623d229cc1feaa9d8794
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-03-12 22:43:39 +0100

    move tests for correct coverage

--- kmeans/src/test/java/kmeans/util/dataloader/TextLoaderStrategyTest.java
+++ kmeans/src/test/java/kmeans/util/dataloader/TextLoaderStrategyTest.java
@@ -1,9 +1,10 @@
-package src.test.java.kmeans.util.dataloader;
+package kmeans.util.dataloader;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+
 import util.dataloader.DataLoader;
 
 public class TextLoaderStrategyTest {
